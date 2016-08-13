cf32ddabe1beb91926e322c6fe2b4baf370dfcc5
jb76 <jorgeb1976@outlook.com>
2016-08-13 09:41:53 -0600

    NumeroPositivoNegativio

---
+++ NumeroPositivoNegativo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroPositivoNegativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa un numero");
+            int numero = Convert.ToInt16(Console.ReadLine());
+            {
+                if (numero > 0)
+                    Console.WriteLine("El numero " + numero + " es positivo");
+                else
+                    Console.WriteLine("El numero " + numero + " es negativo");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
+
