86c7af98d92cc4fa8511b9b3edc8772f4c468cb5
jb76 <jorgeb1976@outlook.com>
2016-08-13 09:35:08 -0600

    Estatura

---
+++ EstaturaPersona/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstaturaPersona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num; string linea;
+            Console.Write("Ingrese su altura: ");
+            linea = Console.ReadLine();
+            num = int.Parse(linea);
+            if (num < 150)
+            {
+                Console.Write("Es de altura baja: ");
+            }
+            else
+            {
+                Console.Write("Es de altura promedio: ");
+            }
+            if (num > 170)
+            {
+                Console.Write("Es de una altura mayor a la promedio: ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
