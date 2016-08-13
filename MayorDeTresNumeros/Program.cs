014a0d029aefe4351604065baebd79f67d5e8365
jb76 <jorgeb1976@outlook.com>
2016-08-13 09:35:54 -0600

    Mayor

---
+++ MayorDeTresNumeros/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicios_3_semana_3_algoritmos
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2, numero3, mayor;
+            
+            Console.Write("Ingrese Numero 1:");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese Numero 2:");
+            numero2 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese Numero 3:");
+            numero3 = int.Parse(Console.ReadLine());
+            
+            if (numero1 > numero2 && numero1 > numero3)
+                mayor = numero1;
+            else if (numero2 > numero1 && numero2 > numero3)
+                mayor = numero2;
+            else
+                mayor = numero3;
+            
+            Console.WriteLine("El Mayor es :{0}", mayor);
+            Console.Read();
+        }
+    }
+}
+
+
