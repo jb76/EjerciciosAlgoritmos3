b56a01ba7c9c99b4d28e0b32df06b63bdfcfbfe2
jb76 <jorgeb1976@outlook.com>
2016-08-13 09:47:27 -0600

    Bisiesto

---
+++ AnioBisiesto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace AnioBisiesto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Ingresa año");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a % 4 == 0 && (a % 100 != 0 || a % 400 == 0))
+            {
+                Console.WriteLine("El año " + a + " Si es bisiesto ");
+            }
+            else
+            {
+                Console.WriteLine("El año " + a + " No es bisiesto ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
