2bb840fc09cbf707282606143d31d2359cac2323
jb76 <jorgeb1976@outlook.com>
2016-08-13 09:42:33 -0600

    NumerosEnteros

---
+++ NumerosEnterosIguales/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosEnterosIguales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero1;
+            float numero2;
+            Console.WriteLine("Escribe el primer valor");
+            numero1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Escribe el segundo valor");
+            numero2 = Convert.ToInt16(Console.ReadLine());
+            if (numero1 == numero2)
+                Console.WriteLine("Ambos son iguales");
+            if (numero1.CompareTo(numero2) > 0)
+                Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+
+}
+    
+
