207840f550795bb1e3218c3e7d5d260dbb0fb8d0
jb76 <jorgeb1976@outlook.com>
2016-08-13 09:33:01 -0600

    Candidato

---
+++ CandidatoVoto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandidatoVoto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad1;
+            
+            string teclado;
+
+            System.Console.WriteLine("\t*** Elegible Para votar ***\n\n");
+            System.Console.WriteLine("Ingresa  edad: ");
+            teclado = System.Console.ReadLine();
+            edad1 = int.Parse(teclado);        
+            System.Console.Write("\nFelicidades puedes Votar: ");
+            System.Console.ReadLine();
+
+
+        }
+    }
+}
